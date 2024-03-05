96c29caa3deccb7489cbd00c8922292bf079fa12
IB Didar <73778140+Didar1313@users.noreply.github.com>
2024-03-05 21:40:47 +0600

    Add files via upload

---
+++ optionalVarriables.dart
@@ -0,0 +1,19 @@
+void main(){
+  String? someValue;
+  print(someValue);
+  
+  someValue="IB Didar";
+  print(someValue);
+  print(someValue.length);
+
+  someValue= null;
+  print(someValue);
+  //print(someValue.length); /shows error.
+  
+  //String someValue2=null; can't declare null directly with datatype.
+  Null someValue2=null;
+  print(someValue2);
+
+
+
+}
