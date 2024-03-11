582b1c832dce1ad5a4d05d72e6d56973a7635a16
IB Didar <73778140+Didar1313@users.noreply.github.com>
2024-03-11 18:20:10 +0600

    Add files via upload

---
+++ controlFlowsOperator.dart
@@ -0,0 +1,16 @@
+void main() {
+  var login = "Manager";
+  var nav = [
+    'Home',
+    'Furniture',
+    'Plants',
+    if (login case "Manager") 'Inventory'
+  ];
+  print(nav);
+  print(nav[0]=="House");
+
+  var listOfInts = [1, 2, 3];
+  var listOfStrings = ['#0', for (var i in listOfInts) '$i'];
+  print(listOfStrings);  
+  print(listOfStrings[1] == '1');
+}
