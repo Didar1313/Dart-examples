efa625b448e4809fe6745448cf71e8fb78f15d9a
IB Didar <73778140+Didar1313@users.noreply.github.com>
2024-03-11 23:16:53 +0600

    Add files via upload

---
+++ metaData.dart
@@ -0,0 +1,17 @@
+void main(){
+  Television television=Television();
+  television.turnOn();
+}
+class Television {
+  /// Use [turnOn] to turn the power on instead.
+  @Deprecated('Use turnOn instead')
+  void activate() {
+    turnOn();
+  }
+
+  /// Turns the TV's power on.
+  void turnOn() {
+    print("Turn On, let's watching el-classico");
+  }
+  // ···
+}
